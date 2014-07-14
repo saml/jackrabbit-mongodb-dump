8180d7845744aa830ed9d1c7120dd99922e920ff
saml <skynare@gmail.com>
2014-07-14 16:23:16 -0400

    can login

---
+++ build.sbt
@@ -0,0 +1,5 @@
+libraryDependencies ++= Seq(
+  "javax.jcr" % "jcr" % "2.0" % "compile",
+  "org.apache.jackrabbit" % "jackrabbit-core" % "2.8.0" withSources(),
+  "org.apache.jackrabbit" % "jackrabbit-jcr2dav" % "2.8.0"
+)
--- src/main/scala/saml/Jcr.scala
+++ src/main/scala/saml/Jcr.scala
@@ -1,12 +1,15 @@
 package saml
 
+import javax.jcr.SimpleCredentials
+
+import org.apache.jackrabbit.commons.JcrUtils
+
 /**
  * author: saml
  */
 case class Jcr(url: String = "http://localhost:4502/crx/server", workspace: String = "crx.default",
                username: String = "admin", password: String = "admin") {
-  private def getRepository() = {
 
-  }
-
+  lazy val repo = JcrUtils.getRepository(url)
+  lazy val session = repo.login(new SimpleCredentials(username, password.toCharArray))
 }
